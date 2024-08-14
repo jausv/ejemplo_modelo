d4ff519f71b164705c32786b7aa840970ac4a3f8
jausv <jausv37@gmail.com>
2024-08-14 16:31:10 -0500

    Cree la vista de los clientes y productos
    Siguiendo las pautas del dto

---
+++ tienda/productos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import listar_clientes, listar_productos
+
+urlpatterns = [
+path('clientes/', listar_clientes, name='listar_clientes'),
+path('productos/', listar_productos, name='listar_productos'),
+]
---
+++ tienda/productos/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Cliente
+
+from .models import Producto
